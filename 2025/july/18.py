d601dbcfdc4521f7a9d56afe534f6cca52edadc2
goldenstein64 <goldnstein64@gmail.com>
2025-07-18 22:30:14 -0400

    feat(2025/07/18): add solution

---
+++ 2025/july/18.py
@@ -0,0 +1,62 @@
+"""
+Implement a class for a stack that supports all the regular functions (push,
+pop) and an additional function of max() which returns the maximum element in
+the stack (return None if the stack is empty). Each method should run in
+constant time.
+"""
+
+import unittest
+
+
+class MaxStack:
+    """
+    Implemented with two stacks: one stores the actual number, and the other
+    stores what the max value was when the last value was pushed.
+    """
+
+    def __init__(self):
+        self.stack: list[int] = []
+        self.max_stack: list[int] = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.max_stack) > 0:
+            self.max_stack.append(max(self.max_stack[-1], val))
+        else:
+            self.max_stack.append(val)
+
+    def pop(self) -> int:
+        self.max_stack.pop()
+        return self.stack.pop()
+
+    def max(self) -> int | None:
+        if len(self.max_stack) > 0:
+            return self.max_stack[-1]
+        else:
+            return None
+
+
+class Tests(unittest.TestCase):
+    def test_smoke(self):
+        s = MaxStack()
+        s.push(1)
+        s.push(2)
+        s.push(3)
+        s.push(2)
+        self.assertEqual(3, s.max())
+        s.pop()
+        s.pop()
+        self.assertEqual(2, s.max())
+
+    def test_empty_pop(self):
+        s = MaxStack()
+        with self.assertRaises(IndexError):
+            s.pop()
+
+    def test_empty_max(self):
+        s = MaxStack()
+        self.assertIsNone(s.max())
+
+
+if __name__ == "__main__":
+    unittest.main()
