d01a7131564fd75a34c83d455033556e98d88ab1
goldenstein64 <goldnstein64@gmail.com>
2025-07-31 12:04:40 -0400

    feat(2025/07/31): add functional implementation

--- 2025/july/31.py
+++ 2025/july/31.py
@@ -19,6 +19,29 @@ Example:
 """
 
 import unittest
+from dataclasses import dataclass, field
+from functools import reduce
+from itertools import product
+from typing import Callable
+
+
+@dataclass(frozen=True)
+class WitnessState:
+    rolling_max: int = 0
+    result: list[int] = field(default_factory=list[int])
+
+
+def witnesses_func(heights: list[int]) -> list[int]:
+    new_state: WitnessState = reduce(
+        lambda state, height: (
+            state
+            if height <= state.rolling_max
+            else WitnessState(rolling_max=height, result=state.result + [height])
+        ),
+        reversed(heights),
+        WitnessState(),
+    )
+    return [*reversed(new_state.result)]
 
 
 def witnesses(heights: list[int]) -> list[int]:
@@ -33,6 +56,8 @@ def witnesses(heights: list[int]) -> list[int]:
 
 
 class Tests(unittest.TestCase):
+    solutions: list[Callable[[list[int]], list[int]]] = [witnesses, witnesses_func]
+
     cases: list[tuple[list[int], list[int]]] = [
         ([], []),
         ([0], []),
@@ -45,9 +70,10 @@ class Tests(unittest.TestCase):
     ]
 
     def test_all(self):
-        for heights, expected in self.cases:
-            with self.subTest(heights=heights, expected=expected):
-                self.assertEqual(expected, witnesses(heights))
+        for solution, (heights, expected) in product(self.solutions, self.cases):
+            name = solution.__name__
+            with self.subTest(solution=name, heights=heights, expected=expected):
+                self.assertEqual(expected, solution(heights))
 
 
 if __name__ == "__main__":
