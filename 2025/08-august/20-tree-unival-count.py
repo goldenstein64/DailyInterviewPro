a5e3b28b9b7aed1e288cfd5fe306359285380168
goldenstein64 <goldnstein64@gmail.com>
2025-08-20 12:57:53 -0400

    feat(2025/08/20): add solution

---
+++ 2025/08-august/20-tree-unival-count.py
@@ -0,0 +1,89 @@
+"""
+A unival tree is a tree where all the nodes have the same value. Given a binary
+tree, return the number of unival subtrees in the tree.
+
+Example:
+
+>>> count_unival_subtrees(
+...     Node(
+...         val=0,
+...         left=Node(1),
+...         right=Node(
+...             val=0,
+...             left=Node(1, Node(1), Node(1)),
+...             right=Node(0),
+...         ),
+...     ),
+... )
+5
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+type TupleTree = tuple[int, TupleTree, TupleTree] | tuple[int, TupleTree] | int
+
+
+@dataclass
+class Node:
+    val: int
+    left: Node | None = None
+    right: Node | None = None
+
+    def str_buffer(self, buffer: list[str], depth: int) -> None:
+        indent = "  " * depth
+        if self.right:
+            self.right.str_buffer(buffer, depth + 1)
+            buffer.append(f"{indent} /")
+
+        buffer.append(f"{indent}{self.val}")
+
+        if self.left:
+            buffer.append(f"{indent} \\")
+            self.left.str_buffer(buffer, depth + 1)
+
+    def __str__(self) -> str:
+        buffer: list[str] = []
+
+        self.str_buffer(buffer, 0)
+
+        return "\n".join(buffer)
+
+
+@dataclass
+class UnivalProperties:
+    unival: int | None
+    count: int
+
+
+def collect_unival(root: Node) -> UnivalProperties:
+    val: int | None = root.val
+    count: int = 0
+    self_count: int = 1
+    if root.left:
+        left = collect_unival(root.left)
+        count += left.count
+        if left.unival is None or left.unival != val:
+            val = None
+            self_count = 0
+
+    if root.right:
+        right = collect_unival(root.right)
+        count += right.count
+        if right.unival is None or right.unival != val:
+            val = None
+            self_count = 0
+
+    return UnivalProperties(val, count + self_count)
+
+
+def count_unival_subtrees(root: Node) -> int:
+    return collect_unival(root).count
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
