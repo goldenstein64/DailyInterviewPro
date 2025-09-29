8df54e1ac6ca8d7c28c2686beb2bcfd2307f2aad
goldenstein64 <goldnstein64@gmail.com>
2025-09-29 02:48:43 -0400

    refactor(2025/08/17): replace binary tree implementation

--- solutions/_2025/_08_august/_17_binary_search_tree_validate.py
+++ solutions/_2025/_08_august/_17_binary_search_tree_validate.py
@@ -8,49 +8,13 @@ root, and all values in the right subtree are greater than or equal to the root.
 from __future__ import annotations
 
 import unittest
-from dataclasses import dataclass
 
-type TupleNode = (
-    tuple[int]
-    | tuple[TupleNode, int]
-    | tuple[int, TupleNode]
-    | tuple[TupleNode, int, TupleNode]
-)
+from ds.binary_tree import BinaryTree, TupleBinaryTree
 
 
-@dataclass
-class Node:
-    val: int
-    left: Node | None = None
-    right: Node | None = None
-
-    @staticmethod
-    def from_tuples(tuples: TupleNode) -> Node:
-        match tuples:
-            case (int(val),):
-                return Node(val, None, None)
-            case (tuple(left), int(val)):
-                return Node(val, Node.from_tuples(left), None)
-            case (int(val), tuple(right)):
-                return Node(val, None, Node.from_tuples(right))
-            case (tuple(left), int(val), tuple(right)):
-                return Node(val, Node.from_tuples(left), Node.from_tuples(right))
-
-    def as_tuples(self) -> TupleNode:
-        match self:
-            case Node(val, None, None):
-                return (val,)
-            case Node(val, Node() as left, None):
-                return (left.as_tuples(), val)
-            case Node(val, None, Node() as right):
-                return (val, right.as_tuples())
-            case Node(val, Node() as left, Node() as right):
-                return (left.as_tuples(), val, right.as_tuples())
-            case _:
-                raise ValueError("unknown Node structure")
-
-
-def is_bst(root: Node | None, lo: int | None = None, hi: int | None = None) -> bool:
+def is_bst(
+    root: BinaryTree[int] | None, lo: int | None = None, hi: int | None = None
+) -> bool:
     if root is None:
         return True
 
@@ -64,7 +28,7 @@ def is_bst(root: Node | None, lo: int | None = None, hi: int | None = None) -> b
 
 
 class Tests(unittest.TestCase):
-    cases: list[tuple[TupleNode | None, bool]] = [
+    cases: list[tuple[TupleBinaryTree[int] | None, bool]] = [
         (None, True),
         ((1,), True),
         (((1,), 1), True),
@@ -88,7 +52,7 @@ class Tests(unittest.TestCase):
                 if root is None:
                     self.assertEqual(expected, is_bst(None))
                 else:
-                    self.assertEqual(expected, is_bst(Node.from_tuples(root)))
+                    self.assertEqual(expected, is_bst(BinaryTree.from_tuples(root)))
 
 
 if __name__ == "__main__":
