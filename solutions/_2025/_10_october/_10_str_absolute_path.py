14d51101587712e5e92a8cb5ad60ae8d512937a5
goldenstein64 <goldnstein64@gmail.com>
2025-10-12 09:37:04 -0400

    feat(2025/10/10): add solution

---
+++ solutions/_2025/_10_october/_10_str_absolute_path.py
@@ -0,0 +1,33 @@
+"""
+Given a file path with folder names, '..' (Parent directory), and '.' (Current
+directory), return the shortest possible file path (Eliminate all the '..' and
+'.').
+
+Example:
+
+>>> shortest_path("/Users/Joma/Documents/../Desktop/./../")
+'/Users/Joma/'
+"""
+
+
+def shortest_path(file_path: str) -> str:
+    if file_path[0] == "/":
+        file_path = file_path[1:]
+
+    if file_path[-1] == "/":
+        file_path = file_path[:-1]
+
+    result: list[str] = []
+    for folder in file_path.split("/"):
+        if folder == "..":
+            result.pop()
+        elif folder != ".":
+            result.append(folder)
+
+    return f"/{"/".join(result)}/"
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
