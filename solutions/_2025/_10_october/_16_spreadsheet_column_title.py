28fa9a521eeeceec990dfacc58afbd05fa068444
goldenstein64 <goldnstein64@gmail.com>
2025-10-16 10:59:00 -0400

    feat(2025/10/16): add solution

---
+++ solutions/_2025/_10_october/_16_spreadsheet_column_title.py
@@ -0,0 +1,12 @@
+"""
+In many spreadsheet applications, the columns are marked with letters. From the
+1st to the 26th column the letters are A to Z. Then starting from the 27th
+column it uses `AA`, `AB`, ..., `ZZ`, `AAA`, etc.
+
+Given a number `n`, find the `n`-th column name.
+"""
+
+# I've already done this problem.
+from _2025._10_october._13_spreadsheet_column_title import (
+    convert_to_title_shorter as column_title,  # type: ignore
+)
