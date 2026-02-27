35ef8d24493be4a7c87492fb4e95350d83a05e18
goldenstein64 <goldnstein64@gmail.com>
2026-02-27 02:07:08 -0500

    feat(2026/02/26): add solution

---
+++ solutions/_2026/_02_february/_26_tree_deepest_node.py
@@ -0,0 +1,37 @@
+r"""
+You are given the root of a binary tree. Return the deepest node (the furthest
+node from the root).
+
+Example:
+
+>>> #     a
+>>> #    / \
+>>> #   b   c
+>>> #  /
+>>> # d
+>>> root = BinaryTree.from_tuples(((("d",), "b"), "a", ("c",)))
+>>> deepest_node(root)
+('d', 3)
+"""
+
+from ds.binary_tree import BinaryTree
+
+
+def deepest_node[T](root: BinaryTree[T], depth: int = 1) -> tuple[T, int]:
+    match (root.left, root.right):
+        case (None, None):
+            return (root.val, depth)
+        case (child, None) | (None, child):
+            return deepest_node(child, depth + 1)
+        case (left, right):
+            return max(
+                deepest_node(left, depth + 1),
+                deepest_node(right, depth + 1),
+                key=lambda t: t[1],
+            )
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
