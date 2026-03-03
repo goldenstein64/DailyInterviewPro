de76bc3f51fc056a258364391f7b936c0fd46455
goldenstein64 <goldnstein64@gmail.com>
2026-03-02 20:45:46 -0500

    feat(2026/03/02): add solution

---
+++ solutions/_2026/_03_march/_02_str_distance_from_char.py
@@ -0,0 +1,45 @@
+"""
+Given a string `s` and a character `c`, find the distance for all characters in
+the string to the character `c` in the string `s`. You can assume that the
+character `c` will appear at least once in the string.
+
+Example:
+
+>>> distance_from_char("helloworld", "l")
+[2, 1, 0, 0, 1, 2, 2, 1, 0, 1]
+
+>>> distance_from_char("gerald", "g")
+[0, 1, 2, 3, 4, 5]
+
+>>> distance_from_char("signage", "e")
+[6, 5, 4, 3, 2, 1, 0]
+"""
+
+
+def distance_from_char(s: str, c: str) -> list[int]:
+    """
+    For every character in `s`, return its distance to the nearest character
+    `c`. It first iterates through the string to get its distance to the
+    nearest character `c` left of it, and then does the same iterating
+    backwards, taking the minimum.
+    """
+    if not s:
+        return []
+    elif s == c:
+        return [0]
+
+    result: list[int] = list(range(len(s))) if s[0] == c else [len(s)] * len(s)
+
+    for i in range(1, len(s)):
+        result[i] = 0 if s[i] == c else result[i - 1] + 1
+
+    for i in reversed(range(len(s) - 1)):
+        result[i] = min(result[i], result[i + 1] + 1)
+
+    return result
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
