708c977517c6b14d46212e7e2319380470e0b773
goldenstein64 <goldnstein64@gmail.com>
2026-03-11 00:47:33 -0400

    doc(2026/03/09): reference old solutions

--- solutions/_2026/_03_march/_09_int_reversed.py
+++ solutions/_2026/_03_march/_09_int_reversed.py
@@ -11,6 +11,12 @@ Examples:
 -123
 """
 
+# This has been done before!
+from solutions._2026._01_january._05_int_reverse import reverse_integer as reversed_old
+from solutions._2025._09_september._10_reverse_int32 import (
+    reverse_int32 as reversed_old2,
+)
+
 
 def reversed(value: int) -> int:
     abs_value: int = abs(value)
@@ -25,6 +31,11 @@ def reversed(value: int) -> int:
 
 
 if __name__ == "__main__":
+    from typing import cast, Any
     import doctest
 
+    doctest.testmod()
+    reversed = cast(Any, reversed_old)
+    doctest.testmod()
+    reversed = cast(Any, reversed_old2)
     doctest.testmod()
